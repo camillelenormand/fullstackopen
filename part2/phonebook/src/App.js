f283b5cadf6d03bbe5505da0699baff6c19019a6
Camille Lenormand <camille.lenormand02@gmail.com>
2023-07-21 16:03:54 +0200

    phonebook step 4 (2.9)

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -3,15 +3,24 @@ import { useState } from 'react'
 const App = () => {
   const [persons, setPersons] = useState(
     [
-      { 
-        name: 'Arto Hellas',
-        phone: '39-44-42424'
-      }
+      { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+      { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+      { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
     ]
   ) 
 
-  const [newName, setNewName] = useState('')
+  const [newName, setNewName, ] = useState('')
   const [newPhoneNumber, setNewPhoneNumber] = useState('')
+  const [newSearch, setNewSearch] = useState('')
+
+  const handleSearch = (e) => {
+    const input = e.target.value
+    setNewSearch(input)
+  }
+
+  const results = persons.filter(person => {
+    return person.name.toLowerCase().includes(newSearch.toLowerCase())
+  })
 
   const handleChangeName = (e) => {
     const newValue = e.target.value
@@ -31,7 +40,7 @@ const App = () => {
     } else {
       const newPerson = {
         name: newName,
-        phone: newPhoneNumber
+        number: newPhoneNumber
       }
       setPersons([...persons, newPerson])
     }
@@ -42,16 +51,23 @@ const App = () => {
   return (
     <div>
       <h2>Phone Book</h2>
+      <h3>Search</h3>
+        <input 
+          placeholder='Enter a name'
+          value={newSearch}
+          onChange={handleSearch}
+        />
+      <h3>New Contact</h3>
       <form onSubmit={handleSubmit}>
         <div>
-          Name: 
+          <label>Name</label>
           <input 
             value={newName}
             onChange={handleChangeName}
           />
         </div>
         <div>
-          Phone: 
+          <label>Phone</label>
           <input 
             value={newPhoneNumber}
             onChange={handleChangePhoneNumber}
@@ -62,11 +78,10 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      <div>debug: {newName}</div>
-        {persons.map((person) => {
+        {results.map((person) => {
           return (
             <div key={person.name}>
-              Name: <b>{person.name}</b> - Phone: <b>{person.phone}</b>
+              Name: <b>{person.name}</b> - Phone: <b>{person.number}</b>
             </div>
           )
         })}
