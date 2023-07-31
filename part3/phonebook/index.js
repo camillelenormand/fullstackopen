40c2498362f8991e59ce0de8a55ba6d891906fe4
Camille Lenormand <camille.lenormand02@gmail.com>
2023-07-31 08:42:34 +0200

    3.5: Phonebook backend step5

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -7,8 +7,7 @@ let persons = [
   {
     id: 1,
     name: 'Arto Hellas',
-    number: '040-123456',
-    age : 10
+    number: '040-123456'
   },
   {
     id: 2,
@@ -78,7 +77,34 @@ app.delete('/api/persons/:id', (request, response) => {
   response.status(204).end();
 })
 
+// Create 1 person
 
+const generateId = () => {
+  const maxId = persons.length > 0
+  ? Math.max(...persons.map(n => n.id))
+  : 0
+  return maxId + 1
+}
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body;
+
+  if (!body.name || !body.number) {
+    response.status(400).json({
+      error: 'content missing'
+    })
+    return
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId(),
+  }
+
+  persons = persons.concat(person)
+  response.json(person)
+})
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
