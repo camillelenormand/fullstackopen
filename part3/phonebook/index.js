3ed68c9d5dbe598b0463e3595e49d58885b9ac75
Camille Lenormand <camille.lenormand02@gmail.com>
2023-07-31 08:30:29 +0200

    3.3: Phonebook backend step3

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -7,7 +7,8 @@ const persons = [
   {
     id: 1,
     name: 'Arto Hellas',
-    number: '040-123456'
+    number: '040-123456',
+    age : 10
   },
   {
     id: 2,
@@ -36,9 +37,39 @@ app.get('/api/persons', (request, response) => {
 // Information about Phonebook
 
 app.get('/info', (request, response) => {
-  response.send(
-    `<p>Phonebook has info for ${persons.length} people</p><br/><p>${new Date()}</p>`)
+  try {
+    const contactCount = persons.length;
+    const contactList = persons.map(person => `<li>Name: ${person.name}</li><li> Phone number: ${person.number}</li>`).join('');
+    const currentDate = new Date();
+
+    const htmlResponse = `
+      <p> Phonebook contains ${contactCount} contacts. </p>
+      <ul>
+        ${contactList}
+      </ul>
+      <br/>
+      <p> ${currentDate} </p>
+    `;
+
+    response.send(htmlResponse);
+  } catch (error) {
+    response.status(500).send('An error occurred while retrieving the phonebook information.');
+  }
+});
+
+//Get 1 person
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id);
+  const person = persons.find(person => person.id === id);
+  if (person) {
+    response.json(person);
+  }
+  else {
+    response.status(404).end();
+  }
 })
+
 
 
 const PORT = process.env.PORT || 3001
