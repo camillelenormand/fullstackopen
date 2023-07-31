3becda04c116df3aaf00b94e301ee57bc7d7fc71
Camille Lenormand <camille.lenormand02@gmail.com>
2023-07-31 09:24:29 +0200

    3.8*: Phonebook backend step8

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -6,6 +6,13 @@ const app = express()
 app.use(express.json())
 app.use(morgan('tiny'))
 
+morgan.token('req-body', (req) => {
+  return JSON.stringify(req.body);
+});
+
+app.use(morgan(':method :url :status :response-time ms - :res[content-length] - :req-body'));
+
+
 let persons = [
   {
     id: 1,
