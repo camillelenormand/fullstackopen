f27905150020e49820bd2f93891ad1b5be236cd3
Camille Lenormand <camille.lenormand02@gmail.com>
2023-08-03 11:47:39 +0200

    update 3.12: Command-line database

--- part3/phonebook/mongo.js
+++ part3/phonebook/mongo.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 
+// Check console input
 if (process.argv.length<3) {
   console.log('give password as argument')
   process.exit(1)
@@ -7,16 +8,19 @@ if (process.argv.length<3) {
 
 const password = process.argv[2]
 
+console.log(process.argv[4])
+console.log(process.argv[3])
 
 const url =
   `mongodb+srv://camille-lenormand:${password}@cluster0.52j61a0.mongodb.net/?retryWrites=true&w=majority`
 
-  console.log('URL',url)
-
+// Connect to MongoDB
 mongoose.set('strictQuery',false)
 mongoose.connect(url)
 
+console.log('mongoose connected')
 
+// Define a schema
 const contactSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -28,14 +32,37 @@ const contactSchema = new mongoose.Schema({
   }
 })
 
+// Define a model for contact
 const Contact = mongoose.model('Contact', contactSchema)
 
-const contact = new Contact({
-  name: 'John Doe',
-  number: '1234567890'
-})
+switch (process.argv.length) {
+  // If arguments length === 3, display contacts
+  case 3:
+    console.log('phonebook:')
+    Contact.find({}).then(result => {
+      result.forEach(contact => {
+        console.log(`${contact.name} ${contact.number}`)
+      })
+      mongoose.connection.close()
+    })
+    break
+    // If arguments length === 5, add contact
+  case 5:
+    console.log('add contact')
+    const name = process.argv[3]
+    const number = process.argv[4]
+    const contact = new Contact({
+      name,
+      number
+    })
+    contact.save().then(result => {
+      console.log(`added ${result.name} number ${result.number} to phonebook`)
+      mongoose.connection.close()
+    })
+    break
+    // Other cases
+  default:
+    console.log('Your console input does not match arguments')
+    process.exit(1)
+}
 
-contact.save().then(result => {
-  console.log(`added ${result.name} number ${result.number} to phonebook`)
-  mongoose.connection.close()
-})
