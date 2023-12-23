238d938474730613fc4ecab2fef0ef6b687124cd
Camille Lenormand <camille.lenormand02@gmail.com>
2023-12-23 18:58:30 +0100

    6.10 Better anecdotes, step8

--- part6/redux-anecdotes/src/reducers/anecdoteFilterReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteFilterReducer.js
@@ -1,23 +1,35 @@
 // reducers/anecdoteFilterReducer.js 
+import { createSlice } from '@reduxjs/toolkit'
 
 // reducer
-const anecdoteFilterReducer = (state = null, action) => {
-  console.log(state)
-  console.log(action)
-  switch (action.type) {
-    case 'SET_FILTER':
-      return action.payload
-    default:
-      return state
-  }
-}
+// const anecdoteFilterReducer = (state = null, action) => {
+//   console.log(state)
+//   console.log(action)
+//   switch (action.type) {
+//     case 'SET_FILTER':
+//       return action.payload
+//     default:
+//       return state
+//   }
+// }
 
 // action
-export const filterChange = (filter) => {
-  return {
-    type: 'SET_FILTER',
-    payload: filter
-  }
-}
+// export const filterChange = (filter) => {
+//   return {
+//     type: 'SET_FILTER',
+//     payload: filter
+//   }
+// }
 
-export default anecdoteFilterReducer
+const filterSlice = createSlice({
+  name: 'filter',
+  initialState: '',
+  reducers: {
+    filterChange(state, action) {
+      return action.payload
+    }
+  }
+})
+
+export const { filterChange } = filterSlice.actions
+export default filterSlice.reducer
