11277b8f5b0af1537296c73c0bf52a42ee3d51a9
Camille Lenormand <camille.lenormand02@gmail.com>
2023-12-29 23:29:05 +0100

    change css

--- part6/redux-anecdotes/src/components/AnecdoteList.jsx
+++ part6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -3,50 +3,51 @@ import { voteAnecdote } from '../reducers/anecdoteReducer'
 import PropTypes from 'prop-types'
 import { displayNotification } from '../reducers/anecdoteNotificationReducer'
 
+// Component to display a single anecdote
 const Anecdote = ({ anecdote, handleClick }) => {
   return (
     <>
       <div>
-      < br/>
-        <i> - {anecdote.content}</i>
+        <br />
+        <i> - {anecdote.content}</i> {/* Display the anecdote content */}
       </div>
-      < br/>
-      <button onClick={handleClick}> {anecdote.votes} Votes </button>
+      <br />
+      <button onClick={handleClick}> {anecdote.votes} Votes </button> {/* Button to vote on the anecdote */}
     </>
   )
 }
 
+// Component to display a list of anecdotes
 const Anecdotes = () => {
   const dispatch = useDispatch()
 
-  // Assuming anecdotes are stored under 'anecdotes' in the Redux state
+  // Retrieve anecdotes and filter state from the Redux store
   const anecdotes = useSelector(state => state.anecdotes)
-  console.log(anecdotes)
   const filter = useSelector(state => state.filter)
-  console.log(filter)
 
-  // Filter and sort anecdotes
+  // Apply filter and sort the anecdotes based on the number of votes
   const filteredAnecdotes = anecdotes
-  .filter(anecdote => anecdote.content.toLowerCase().includes(filter.toLowerCase()))
-  .sort((a, b) => b.votes - a.votes)
+    .filter(anecdote => anecdote.content.toLowerCase().includes(filter.toLowerCase()))
+    .sort((a, b) => b.votes - a.votes)
 
-  console.log(filteredAnecdotes)
-
+  // Display a message if no anecdotes are found after filtering
   if (filteredAnecdotes.length === 0) {
-    return <p>No results found for {filter}.</p>
+    return <p className='notice'>No results found for {filter}.</p>
   }
 
+  // Function to handle voting and displaying notification
   const voteAndNotify = (anecdote) => {
-    dispatch(voteAnecdote(anecdote.id))
+    dispatch(voteAnecdote(anecdote.id)) // Dispatch the voteAnecdote action
       .then(() => {
-      dispatch(displayNotification(`You voted '${anecdote.content}'`, 2))
-     })
-     .catch(err => {
-      console.log(err)
-      dispatch(displayNotification('Error occurred while voting. Please try again.', 10))
-     })
+        dispatch(displayNotification(`You voted '${anecdote.content}'`, 2)) // Show success notification
+      })
+      .catch(err => {
+        console.log(err)
+        dispatch(displayNotification('Error occurred while voting. Please try again.', 10)) // Show error notification
+      })
   }
 
+  // Render a list of Anecdote components
   return (
     <div>
       {filteredAnecdotes.map(anecdote =>
@@ -60,6 +61,7 @@ const Anecdotes = () => {
   )
 }
 
+// Define PropTypes for the Anecdote component
 Anecdote.propTypes = {
   anecdote: PropTypes.shape({
     content: PropTypes.string.isRequired,
