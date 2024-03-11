f20addf3bc3f7f40b2e1753d923447046cae4234
Camille Lenormand <camille.lenormand02@gmail.com>
2024-03-11 08:42:31 +0100

    fix backend api

--- part7/blog-list-more/backend/app.js
+++ part7/blog-list-more/backend/app.js
@@ -1,48 +1,48 @@
-const config = require("./utils/config");
-const express = require("express");
-const app = express();
-const cors = require("cors");
-require("express-async-errors");
-const logger = require("./utils/logger");
-const mongoose = require("mongoose");
+const config = require('./utils/config')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+require('express-async-errors')
+const logger = require('./utils/logger')
+const mongoose = require('mongoose')
 
 // Router
-const blogsRouter = require("./controllers/blogs");
-const usersRouter = require("./controllers/users");
-const loginRouter = require("./controllers/login");
-const testingRouter = require("./controllers/testing");
+const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
+const testingRouter = require('./controllers/testing')
 
-const middleware = require("./utils/middleware");
+const middleware = require('./utils/middleware')
 
-mongoose.set("strictQuery", false);
+mongoose.set('strictQuery', false)
 
-logger.info("connecting to", config.MONGODB_URI);
+logger.info('connecting to', config.MONGODB_URI)
 
 mongoose
-  .connect(config.MONGODB_URI)
-  .then(() => {
-    logger.info("connected to MongoDB");
-  })
-  .catch((error) => {
-    logger.error("error connecting to MongoDB:", error.message);
-  });
+	.connect(config.MONGODB_URI)
+	.then(() => {
+		logger.info('connected to MongoDB')
+	})
+	.catch((error) => {
+		logger.error('error connecting to MongoDB:', error.message)
+	})
 
-app.use(cors());
-app.use(express.static("build"));
-app.use(express.json());
+app.use(cors())
+app.use(express.static('build'))
+app.use(express.json())
 
 // Routes
-app.use("/api/blogs", blogsRouter);
-app.use("/api/users", usersRouter);
-app.use("/api/login", loginRouter);
-app.use("/api/testing", testingRouter);
+app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
+app.use('/api/testing', testingRouter)
 
-app.use(middleware.unknownEndpoint);
-app.use(middleware.errorHandler);
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
-if (process.env.NODE_ENV === "test") {
-  const testingRouter = require("./controllers/testing");
-  app.use("/api/testing", testingRouter);
+if (process.env.NODE_ENV === 'test') {
+	const testingRouter = require('./controllers/testing')
+	app.use('/api/testing', testingRouter)
 }
 
-module.exports = app;
+module.exports = app
--- part7/blog-list-more/backend/controllers/login.js
+++ part7/blog-list-more/backend/controllers/login.js
@@ -1,34 +1,34 @@
-const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
-const loginRouter = require("express").Router();
-const User = require("../models/user");
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
+const loginRouter = require('express').Router()
+const User = require('../models/user')
 
-loginRouter.post("/", async (request, response) => {
-  const { username, password } = request.body;
+loginRouter.post('/', async (request, response) => {
+	const { username, password } = request.body
 
-  const user = await User.findOne({ username });
-  const passwordCorrect =
-    user === null ? false : await bcrypt.compare(password, user.passwordHash);
+	const user = await User.findOne({ username })
+	const passwordCorrect =
+    user === null ? false : await bcrypt.compare(password, user.passwordHash)
 
-  if (!(user && passwordCorrect)) {
-    return response.status(401).json({
-      error: "invalid password or username",
-    });
-  }
+	if (!(user && passwordCorrect)) {
+		return response.status(401).json({
+			error: 'invalid password or username',
+		})
+	}
 
-  const userForToken = {
-    username: user.username,
-    id: user._id,
-  };
+	const userForToken = {
+		username: user.username,
+		id: user._id,
+	}
 
-  // Token expires in 60*60 seconds, i.e 1 hour
-  const token = jwt.sign(userForToken, process.env.SECRET, {
-    expiresIn: 60 * 60,
-  });
+	// Token expires in 60*60 seconds, i.e 1 hour
+	const token = jwt.sign(userForToken, process.env.SECRET, {
+		expiresIn: 60 * 60,
+	})
 
-  response
-    .status(200)
-    .send({ token, username: user.username, name: user.name });
-});
+	response
+		.status(200)
+		.send({ token, username: user.username, name: user.name })
+})
 
-module.exports = loginRouter;
+module.exports = loginRouter
