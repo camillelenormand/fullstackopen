eddddd37eb084a3259363874e2d72eac628c383d
Camille Lenormand <camille.lenormand02@gmail.com>
2024-03-22 18:30:17 +0100

    Refactor BlogList component styling

--- part7/blog-list-more/frontend-redux/src/components/BlogList.jsx
+++ part7/blog-list-more/frontend-redux/src/components/BlogList.jsx
@@ -3,88 +3,93 @@ import { initializeBlogs } from '../store/blogReducer'
 import styled from 'styled-components'
 import { useEffect } from 'react'
 
-
-const BlogContainer = styled.article`
-  font-family: Arial, sans-serif;
-  border: 1px solid #ddd;
-  border-radius: 4px;
-  padding: 16px;
-  margin-bottom: 16px;
-  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
-`;
-
-const BlogTitle = styled.h3`
-  font-family: 'Roboto', sans-serif;
-  font-size: 1.2em;
-  margin-bottom: 8px;
-  font-weight: 500; /* Slightly bolder for emphasis */
+const Table = styled.table`
+  width: 100%;
+  max-width: 800px;
+  margin: 20px auto;
+  border-collapse: collapse;
 `
 
-const Metadata = styled.div`
-  font-family: 'Roboto', sans-serif;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  font-size: 0.9em;
-  color: #90a4ae; /* Google's Material Design grey */
+const Thead = styled.thead`
+  background-color: #f5f5f5;
 `
 
-const Author = styled.p`
-  font-family: 'Roboto', sans-serif;
-  margin-right: 16px;
+const Th = styled.th`
+  padding: 10px;
+  text-align: left;
+  border-bottom: 2px solid #ddd;
 `
 
-const Url = styled.a`
-  font-family: 'Roboto', sans-serif;
-  color: #1a73e8; /* Google blue */
+const Td = styled.td`
+  padding: 10px;
+  border-bottom: 1px solid #ddd;
+`
+
+const Tr = styled.tr`
+  &:nth-child(even) {
+    background-color: #f9f9f9;
+  }
+`
+
+const UrlLink = styled.a`
+  color: #1a73e8;
   text-decoration: none;
-
   &:hover {
     text-decoration: underline;
   }
 `
 
-const Blog = ({ blog }) => {
-  const { author, url, title } = blog
+const LoadingMessage = styled.div`
+  text-align: center;
+  font-size: 1.2em;
+  color: #607d8b;
+`
 
-  return (
-    <BlogContainer>
-      <BlogTitle>{title}</BlogTitle>
-      <Metadata>
-        <Author><i>- {author}</i></Author>
-        <Url href={url} target="_blank" rel="noreferrer noopener">
-          {url}
-        </Url>
-      </Metadata>
-    </BlogContainer>
-  )
-}
+const NoBlogsMessage = styled.div`
+  text-align: center;
+  font-size: 1.2em;
+  color: #ff5722;
+`
 
 const BlogList = () => {
   const dispatch = useDispatch()
-  const { blogs, isLoading } = useSelector(state => state.blogs) // state.blogs is from the blogReducer.js file
-
-  console.log('blogs', blogs)
-  console.log('isLoading', isLoading)
+  const { blogs, isLoading } = useSelector(state => state.blogs)
 
   useEffect(() => {
     dispatch(initializeBlogs())
   }, [dispatch])
 
   return (
-    <div>
+    <>
       {isLoading ? (
-        <div>Loading blogs...</div>
+        <LoadingMessage>Loading blogs...</LoadingMessage>
+      ) : blogs.length > 0 ? (
+        <Table>
+          <Thead>
+            <tr>
+              <Th>Title</Th>
+              <Th>Author</Th>
+              <Th>URL</Th>
+            </tr>
+          </Thead>
+          <tbody>
+            {blogs.map((blog) => (
+              <Tr key={blog.id}>
+                <Td>{blog.title}</Td>
+                <Td>{blog.author}</Td>
+                <Td>
+                  <UrlLink href={blog.url} target="_blank" rel="noreferrer noopener">
+                    Visit
+                  </UrlLink>
+                </Td>
+              </Tr>
+            ))}
+          </tbody>
+        </Table>
       ) : (
-        blogs.length > 0 ? (
-          blogs.map(blog => (
-            <Blog key={blog.id} blog={blog} />
-          ))
-        ) : (
-          <div>No blogs found.</div>
-        )
+        <NoBlogsMessage>No blogs found.</NoBlogsMessage>
       )}
-    </div>
+    </>
   )
 }
 
