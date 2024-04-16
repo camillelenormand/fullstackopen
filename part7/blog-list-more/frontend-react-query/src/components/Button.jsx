05d65a14142e5b8c1bc560537c13233df5cb7858
Camille Lenormand <camille.lenormand02@gmail.com>
2024-04-16 19:00:23 +0200

    Refactor Button component to use styled-components for styling in frontend-react-query/src/components/Button.jsx

--- part7/blog-list-more/frontend-react-query/src/components/Button.jsx
+++ part7/blog-list-more/frontend-react-query/src/components/Button.jsx
@@ -1,10 +1,21 @@
 // Button.jsx
+import styled from 'styled-components'
+
+const StyledButton = styled.button`
+  background-color: ${props => props.theme.secondaryColor};
+  color: ${props => props.theme.text};
+  border: 2px solid ${props => props.theme.toggleBorder};
+
+  &:hover {
+      background-color: ${props => props.theme.secondaryColor};
+  }
+`
 
 const Button = ({ onClick, children, className = '', ...props }) => {
   return (
-    <button onClick={onClick} className={`btn ${className}`} {...props}>
+    <StyledButton onClick={onClick} className={`btn ${className}`} {...props}>
       {children}
-    </button>
+    </StyledButton>
   )
 }
 
