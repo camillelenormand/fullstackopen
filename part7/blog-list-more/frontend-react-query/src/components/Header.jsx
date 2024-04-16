c69a1bc02bfba275afab66ba9b462dba0fd946a3
Camille Lenormand <camille.lenormand02@gmail.com>
2024-04-16 22:34:43 +0200

    Add BlogForm component to Header for creating new blogs in frontend-react-query/src/components/Header.jsx

--- part7/blog-list-more/frontend-react-query/src/components/Header.jsx
+++ part7/blog-list-more/frontend-react-query/src/components/Header.jsx
@@ -4,6 +4,8 @@ import {
 import styled from 'styled-components'
 import LogoutButton from './LogOutButton'
 import Notification from './Notification'
+import { useAuth } from '../contexts/AuthContext'
+import BlogForm from './BlogForm'
 
 const HeaderStyle = styled.header`
   background-color: #f0f0f0;
@@ -12,15 +14,23 @@ const HeaderStyle = styled.header`
 `
 
 const Header = () => {
-  const token = window.localStorage.getItem('loggedBlogToken')
+  const { token } = useAuth()
+
   return (
     <>
       <HeaderStyle>
-      <Notification />
+        <Notification />
         <Link to="/blogs">Blogs</Link>
         <Link to="/users">Users</Link>
         {
-          !token ? <Link to="/login">Login</Link> : <LogoutButton />
+          !token ? (
+            <Link to="/login">Login</Link>
+          ) : (
+            <>
+              <Link to={`/blogs/new`} element={<BlogForm />}>Create a New Blog</Link>
+              <LogoutButton />
+            </>
+          )
         }
       </HeaderStyle>
     </>
