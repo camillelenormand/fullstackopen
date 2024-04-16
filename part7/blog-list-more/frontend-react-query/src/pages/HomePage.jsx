1467c7fc1c58963ff0edf638dc14167baab6ebb8
Camille Lenormand <camille.lenormand02@gmail.com>
2024-04-16 22:34:32 +0200

    Remove BlogForm component from HomePage in frontend-react-query/src/pages/HomePage.jsx

--- part7/blog-list-more/frontend-react-query/src/pages/HomePage.jsx
+++ part7/blog-list-more/frontend-react-query/src/pages/HomePage.jsx
@@ -1,11 +1,9 @@
 import BlogList from '../components/BlogList'
-import BlogForm from '../components/BlogForm'
 
 function HomePage() {
 
     return (
       <div>
-        <BlogForm />
         <BlogList />
       </div>
     )
