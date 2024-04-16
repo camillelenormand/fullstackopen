09066e5236f3b909570144a05915270f49a9dc60
Camille Lenormand <camille.lenormand02@gmail.com>
2024-04-16 19:00:00 +0200

    Add GlobalStyle component to apply global styles in frontend-react-query/src/components/GlobalStyle.jsx

---
+++ part7/blog-list-more/frontend-react-query/src/components/GlobalStyle.jsx
@@ -0,0 +1,24 @@
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    background: ${props => props.theme.body};
+    color: ${props => props.theme.text};
+    font-family: 'Roboto', sans-serif;
+    font-size: 16px;
+    transition: all 0.25s linear;
+  }
+
+  h1, h2, h3, h4, h5, h6 {
+    margin: 0;
+    padding: 0;
+  }
+
+  p {
+    margin: 0;
+    padding: 0;
+  }
+  `
+export default GlobalStyle
