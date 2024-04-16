49564d93cab7155d66e74864d67312c5eeb22cb0
Camille Lenormand <camille.lenormand02@gmail.com>
2024-04-16 22:34:39 +0200

    Remove BlogForm component from BlogPage in frontend-react-query/src/pages/BlogPage.jsx

--- part7/blog-list-more/frontend-react-query/src/pages/BlogPage.jsx
+++ part7/blog-list-more/frontend-react-query/src/pages/BlogPage.jsx
@@ -1,13 +1,9 @@
- import BlogForm from '../components/BlogForm'
 import BlogList from '../components/BlogList'
 
 const BlogPage = () => { 
-  const user = window.localStorage.getItem('loggedBlogUsername')
-  console.log('user:', user)
 
   return (
     <BlogListStyles>
-      <BlogForm />
       <BlogList />
     </BlogListStyles>
   )
