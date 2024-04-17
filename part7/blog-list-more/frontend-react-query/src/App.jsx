f15d122474b767080f9a91315e36e055dac42e87
Camille Lenormand <camille.lenormand02@gmail.com>
2024-04-17 08:12:51 +0200

    Refactor routing in App component to include error handling in frontend-react-query/src/App.jsx

--- part7/blog-list-more/frontend-react-query/src/App.jsx
+++ part7/blog-list-more/frontend-react-query/src/App.jsx
@@ -2,7 +2,6 @@ import HomePage from './pages/HomePage'
 import LoginForm from './components/LoginForm'
 import UserList from './components/UserList'
 import {
-  BrowserRouter as Router,
   Routes, 
   Route, 
 } from 'react-router-dom'
@@ -10,6 +9,7 @@ import User from './components/User'
 import Blog from './components/Blog'
 import Header from './components/Header'
 import BlogForm from './components/BlogForm'
+import Error from './components/Error'
 
 function App() {
 
@@ -17,13 +17,13 @@ function App() {
     <div className="App">
       <Header />
       <Routes>
-        <Route path="/blogs" element={<HomePage />} />
-        <Route path="/login" element={<LoginForm />} />
-        <Route path="/users" element={<UserList/>} />
-        <Route path="blogs/new" element={<BlogForm />} />
-        <Route path="/" element={<HomePage />} />
-        <Route path="/users/:id" element={<User/>} />
-        <Route path="/blogs/:id" element={<Blog/>} />
+        <Route path="/blogs" element={<HomePage />} errorElement={<Error />} />
+        <Route path="/login" element={<LoginForm />} errorElement={<Error />} />
+        <Route path="/users" element={<UserList/>} errorElement={<Error />} />
+        <Route path="blogs/new" element={<BlogForm />} errorElement={<Error />} />
+        <Route path="/" element={<HomePage />} errorElement={<Error />} />
+        <Route path="/users/:id" element={<User/>} errorElement={<Error />} />
+        <Route path="/blogs/:id" element={<Blog/>} errorElement={<Error />} />
       </Routes>
       <footer>
         <p>Blog app April 2024</p>
