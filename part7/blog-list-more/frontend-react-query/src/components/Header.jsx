7a8c6bad36dde6d861ed712a72b7162d8a44a334
Camille Lenormand <camille.lenormand02@gmail.com>
2024-04-19 12:18:16 +0200

    Refactor Header component to use styled components for styling in frontend-react-query/src/components/Header.jsx

--- part7/blog-list-more/frontend-react-query/src/components/Header.jsx
+++ part7/blog-list-more/frontend-react-query/src/components/Header.jsx
@@ -7,32 +7,46 @@ import Notification from './Notification'
 import { useAuth } from '../contexts/AuthContext'
 import BlogForm from './BlogForm'
 
-const HeaderStyle = styled.header`
-  background-color: #f0f0f0;
-  padding: 10px;
-  margin-bottom: 10px;
+const HeaderContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: white;
+  padding: 10px 20px;
+  font-family: 'Roboto', sans-serif;
 `
+
+const Nav = styled.nav`
+  display: flex;
+  gap: 20px;
+`
+
+const NavLink = styled(Link)`
+  color: black;
+  text-decoration: none;
+  font-size: 16px;
+  font-weight: 500;
+
+  &:hover {
+    text-decoration: underline;
+}
+`
+
 
 const Header = () => {
   const { token } = useAuth()
 
   return (
     <>
-      <HeaderStyle>
+      <HeaderContainer>
         <Notification />
-        <Link to="/blogs">Blogs</Link>
-        <Link to="/users">Users</Link>
-        {
-          !token ? (
-            <Link to="/login">Login</Link>
-          ) : (
-            <>
-              <Link to={`/blogs/new`} element={<BlogForm />}>Create a New Blog</Link>
-              <LogoutButton />
-            </>
-          )
-        }
-      </HeaderStyle>
+        <Nav>
+          <NavLink to="/blogs">Blogs</NavLink>
+          <NavLink to="/users">Users</NavLink>
+          {!token ? null : <NavLink to={`/blogs/new`} element={<BlogForm />}>Create a New Blog</NavLink>}
+        </Nav>
+        {!token ? <NavLink to="/login">Login</NavLink> : <LogoutButton />}
+      </HeaderContainer>
     </>
   )
 }
