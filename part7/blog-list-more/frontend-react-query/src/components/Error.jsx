3fd31fcb3cb45af23c831e4f4f011efa2b4c73dc
Camille Lenormand <camille.lenormand02@gmail.com>
2024-05-14 08:53:09 +0200

    chore: Update Error component to use styled-components for styling

--- part7/blog-list-more/frontend-react-query/src/components/Error.jsx
+++ part7/blog-list-more/frontend-react-query/src/components/Error.jsx
@@ -1,8 +1,26 @@
+import styled from "styled-components"
+
+const ErrorContainer = styled.div`
+  color: red;
+  font-size: 1.5rem;
+  font-weight: 500;
+  margin-bottom: 10px;
+  text-align: center;
+`
+
+const ErrorMessage = styled.p`
+  color: white;
+  font-size: 1.5rem;
+  font-weight: 500;
+  margin-bottom: 10px;
+  text-align: center;
+`
+
 const Error = ({ error }) => {
   return (
-    <div>
-      Error: {error.message}
-    </div>
+    <ErrorContainer>
+      <ErrorMessage> {error.message} </ErrorMessage>
+    </ErrorContainer>
   )
 }
 
