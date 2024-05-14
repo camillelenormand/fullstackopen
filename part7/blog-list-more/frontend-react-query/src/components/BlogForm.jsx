35c9af8bf8294cbc004020d727318063abb19926
Camille Lenormand <camille.lenormand02@gmail.com>
2024-05-14 08:54:40 +0200

    refactor: Update BlogForm component to use custom hooks for form fields and authentication

--- part7/blog-list-more/frontend-react-query/src/components/BlogForm.jsx
+++ part7/blog-list-more/frontend-react-query/src/components/BlogForm.jsx
@@ -1,46 +1,53 @@
-import { 
-  BlogFormContainer, 
-  BlogFormInput, 
+import {
+  BlogFormContainer,
+  BlogFormInput,
   BlogFormLabel,
   BlogButton
 } from './BlogFormStyles'
-import useCreateMutation from '../hooks/useCreateMutation'
+import useCreateBlog from '../hooks/useCreateBlog'
 import { useAuth } from '../contexts/AuthContext'
 import { useNotify } from '../contexts/NotificationContext'
+import { useNavigate } from 'react-router-dom'
 
 const BlogForm = () => {
-  const createBlogMutation = useCreateMutation()
-  const user = useAuth()
+  const createBlog = useCreateBlog()
+  const { authState } = useAuth()
   const notify = useNotify()
+  const navigate = useNavigate()
 
-  if (!user) {
-    // Optionally handle the absence of a token, e.g., redirect to login or show a message.
-    console.error('Please log in.')
+  if (!authState) {
     notify('Please log in.')
+    navigate('/login')
   }
 
   const onCreate = async (event) => {
     event.preventDefault()
     const { title, author, url } = event.target.elements
-    console.log('title:', title.value, 'author:', author.value, 'url:', url.value)
 
-    createBlogMutation.mutate({ 
-      title: title.value, 
+    createBlog.mutate({
+      title: title.value,
       author: author.value,
       url: url.value
+    }, {
+      onSuccess: () => {
+        notify('Blog created successfully.')
+        navigate('/')
+      },
+      onError: (error) => {
+        notify(`Failed to create blog: ${error.message}`, 'error')
+        event.target.reset()
+      }
     })
-
-    event.target.reset()
   }
 
   return (
     <BlogFormContainer onSubmit={onCreate}>
       <BlogFormLabel htmlFor='title'>Title</BlogFormLabel>
-      <BlogFormInput id='title' name='title' placeholder='Enter a title' type='text' required/>
+      <BlogFormInput id='title' name='title' placeholder='Enter a title' type='text' aria-label='name' required />
       <BlogFormLabel htmlFor='author'>Author</BlogFormLabel>
-      <BlogFormInput id='author' name='author' placeholder='Enter an author'type='text' required/>
+      <BlogFormInput id='author' name='author' placeholder='Enter an author' type='text' aria-label='name' required />
       <BlogFormLabel htmlFor='url'>URL</BlogFormLabel>
-      <BlogFormInput id='url' name='url' placeholder='Enter an URL' type='text' required/>
+      <BlogFormInput id='url' name='url' placeholder='Enter a URL' type='text' aria-label='url' required />
       <BlogButton type='submit'>Create</BlogButton>
     </BlogFormContainer>
   )
