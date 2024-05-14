d8525bbbba4307411b17157752dd04abf284129e
Camille Lenormand <camille.lenormand02@gmail.com>
2024-05-14 08:52:45 +0200

    chore: Update NoBlogs component to use styled-components for styling

--- part7/blog-list-more/frontend-react-query/src/components/NoBlogs.jsx
+++ part7/blog-list-more/frontend-react-query/src/components/NoBlogs.jsx
@@ -1,9 +1,25 @@
+import styled from 'styled-components'
+
+
+const NoBlogsContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`
+
+const NoBlogsTitle = styled.h2`
+  font-size: 1.5rem;
+  font-weight: 500;
+  margin-bottom: 10px;
+  text-align: center;
+`
 
 const NoBlogs = () => {
   return (
-    <div>
-      <h2>No blogs available</h2>
-    </div>
+    <NoBlogsContainer>
+      <NoBlogsTitle>No blogs available</NoBlogsTitle>
+    </NoBlogsContainer>
   )
 }
 
