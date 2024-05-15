486363ea839127171aa241893aa2581d997808b0
Camille Lenormand <camille.lenormand02@gmail.com>
2024-05-15 08:47:31 +0200

    refactor: Update useDeleteBlog hook to use react-query's useMutation hook for deleting blogs

--- part7/blog-list-more/frontend-react-query/src/hooks/useDeleteBlog.js
+++ part7/blog-list-more/frontend-react-query/src/hooks/useDeleteBlog.js
@@ -1,39 +1,42 @@
 import { useMutation, useQueryClient } from 'react-query'
 import blogService from '../services/blogs'
 import { useNotify } from '../contexts/NotificationContext'
+import { Navigate } from 'react-router-dom'
 
-const useDelete= () => {
+const useDelete = () => {
   const queryKey = ['blogs']
   const queryClient = useQueryClient()
   const notify = useNotify()
-  const token = JSON.parse(window.localStorage.getItem('loggedBlogToken'))
 
-  return useMutation(
-    deletedBlog => blogService.deleteBlog(deletedBlog, token),
-    {
-      onMutate: async (deletedBlog) => {
-        queryClient.setQueryData(queryKey, oldData => {
-          if (oldData && oldData.blogs) {
-            return {
-              ...oldData,
-              blogs: oldData.blogs.filter(blog => blog.id !== deletedBlog.id)
-            }
-          }
-          return oldData
+  return useMutation({
+    mutationFn: blogService.deleteBlog,
+    onMutate: async (deletedBlog) => {
+      await queryClient.cancelQueries(queryKey)
+
+      const previousBlogs = queryClient.getQueryData(queryKey)
+
+      if (previousBlogs) {
+        queryClient.setQueryData(queryKey, {
+          ...previousBlogs,
+          blogs: previousBlogs.blogs.filter(blog => blog.id !== deletedBlog.id)
         })
-      },
-      onSuccess: (data) => {
-        queryClient.invalidateQueries(queryKey)
-        notify('Blog deleted successfully')
-        console.log('Mutation successful with data:', data)
-      },
-      onError: (error, deletedBlog, context) => {
-        queryClient.setQueryData(queryKey, context.previousBlogs)
-        notify(error)
-        console.error('Failed to delete blog: ', error)
       }
+
+      return { previousBlogs }
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries(queryKey)
+      notify('Blog deleted successfully', 'success')
+      Navigate('/')
+    },
+    onError: (error, deletedBlog, context) => {
+      if (context?.previousBlogs) {
+        queryClient.setQueryData(queryKey, context.previousBlogs)
+      }
+      notify(`Failed to delete the blog: ${error.message}`, 'error')
     }
-  )
+  })
 }
+
 
 export default useDelete
