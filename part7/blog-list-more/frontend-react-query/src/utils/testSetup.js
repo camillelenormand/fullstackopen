b839571fce9afbb155b0280548e5be667f432ba6
Camille Lenormand <camille.lenormand02@gmail.com>
2024-05-16 08:43:10 +0200

    refactor: Update testSetup.js to remove unused imports and clean up code formatting

--- part7/blog-list-more/frontend-react-query/src/utils/testSetup.js
+++ part7/blog-list-more/frontend-react-query/src/utils/testSetup.js
@@ -2,11 +2,11 @@ import { afterAll, afterEach, beforeAll } from 'vitest'
 import { cleanup } from '@testing-library/react'
 import '@testing-library/jest-dom/vitest'
 
-
+// Establish API mocking before all tests.
 beforeAll(() => {
 })
 
 afterEach(() => {
-
   cleanup()
 })
+
