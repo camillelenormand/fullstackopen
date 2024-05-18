db24b0a68508d2798e5e75345590ea2d17e36098
Camille Lenormand <camille.lenormand02@gmail.com>
2024-05-18 09:47:28 +0200

    refactor: Remove unused beforeAll function in testSetup.js

--- part7/blog-list-more/frontend-react-query/src/utils/testSetup.js
+++ part7/blog-list-more/frontend-react-query/src/utils/testSetup.js
@@ -1,10 +1,8 @@
-import { afterAll, afterEach, beforeAll } from 'vitest'
+import { afterEach } from 'vitest'
 import { cleanup } from '@testing-library/react'
 import '@testing-library/jest-dom/vitest'
 
 // Establish API mocking before all tests.
-beforeAll(() => {
-})
 
 afterEach(() => {
   cleanup()
